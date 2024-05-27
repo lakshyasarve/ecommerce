a59d5fc55f0723d63b0f76f7a0a3030eaa901c7a
Lakshya Sarve <lakshyasarve@gmail.com>
2024-05-28 01:40:20 +0530

    Scroll to top added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import CustomerRouters from './Routers/CustomerRouters';
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
-
+import ScrollToTop from './customer/components/ScrollToTop';
 
 function App() {
   return (
     <>
      <FloatingWhatsApp accountName='Kiah Hygiene' phoneNumber='9109044644' placeholder='I want to enquire about products' notification = {1} notificationSound = {1} chatMessage='Welcome to Kiah Hygiene! How can we help you?' avatar='/images/Kiah_whatsapp_icon.png'
     />
+    <ScrollToTop></ScrollToTop>
     <Routes>
       <Route path='/*' element={<CustomerRouters></CustomerRouters>}></Route>
     </Routes>
